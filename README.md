af21c19607e70def49468db96a41802474c8a210
Mayong <1345959121@qq.com>
2018-09-06 18:22:16 +0800

    最基本的webpack

--- README.md
+++ README.md
@@ -13,7 +13,14 @@ webpack学习记录
 
 webpack基础
 
-基于node流，采用commonjs模式（require => moudle.explort)
+1. 基于node流，采用commonjs模式（require => moudle.explort)
+2. webpack加载流程，从入口开始，区分依赖和代码，`递归`加载依赖,解决了浏览器端无法识别模块化后的代码的问题。
+3. chunk 和budle的对比
+chunk是代码块的意思
+bundle是最终资源合集的意思
+1个chunk可以是多个模块构成
+这部分在后面代码分割的时候会再讲
+**没有比较清楚的明白，暂时不细究。**
 
 
 webpack 安装
@@ -22,5 +29,12 @@ webpack 安装
 2. webpack4版本需要单独安装命令行
 3. webpack可以通过@制定版本，不过属于node（npm）基础
 4. webpack -v 可检查安装成功  还有npx webpack -v
+
+
+webpack基本使用流程
+ 
+1. 可以零配置使用，但一些路径也需要配合`默认设置`，其中特别之处，源代码为src目录下，源文件名为index.js，打包出的文件路径不用特殊设置，没有的话会自动创建，默认是dist目录下，为main.js。
+2. 也可以自定义配置，需在根目录下添加`webpack.config.js`文件，基于node流的模块，暴露的为一个对象，具体细节见git记录。
+3. 对于最基本的webpack打包来说，只需要设置正确的`entry`和`output`即可。
 
 
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+// node的文件路径处理模块
+const path = require('path')
+module.exports = {
+    // 不常用字段，指定上下文，为相对路径的依据，默认为根目录即process.cwd()。
+    context: process.cwd(),
+    // 决定一些打包设置，已知就是生产模式会压缩代码。默认设置中是生产模式。
+    // prouduction  生产
+    // development  开发
+    // none 不启用构建模式，默认production
+    mode: 'development',
+    // 入口，也就是编译的起点，相对路径即可，任何文件格式都可以，但需要设置对应的rules（webpack只认识js）。
+    // str arr obj
+    // entry: './src/index.js',
+    // 非多入口，为多源文件，最终会是合并进一个main.js。
+    // entry: ['./src/index.js','./src/other.js'],
+    // 多入口的正确实现  
+    // 多入口一定是多出口，多入口情况下不支持自定义指定出口文件名。
+    entry: {
+        index: './src/index.js',
+        other: './src/other.js'
+    },
+    // 出口，最终编译打包完的东西的存放设置。对象形式，包括路径和文件名。
+    output: {
+        // 这个地方需要注意，必须是绝对路径
+        path: path.resolve(__dirname,'./dist'),
+        // filename: 'index.js'
+        // 多出口模式下使用占位符，源文件名相同。
+
+        // 目前支持的占位符
+        // hash  项目hash，主要防缓存，每次打包是weboack—cli里的，每次更新，可以设置长度，最长20位，默认20位，如filename: '[name]-[hash:16].js'。
+        // chunkhash  代码块hash，主要是利用浏览器缓存，尽可能小的更新chunk。根据不同入口形成不同的代码chunck，只要对应入口的内容没有改变，chunckhash不变。
+        // name 对应的entry的key
+        // id
+        filename: '[name]-[chunkhash].js'
+    }
+}
